5656d591d95ff235a7e6b5a7cc24821a3d4d5ece
yexp <yexp@hangzhou.fujisoft-china.com>
2019-08-08 17:39:01 +0800

    init project

---
+++ qfly.cpp
@@ -0,0 +1,17 @@
+﻿#include "qfly.h"
+#include "ui_qfly.h"
+
+QFly::QFly(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::QFly)
+{
+    ui->setupUi(this);
+
+    m_cstSpeedPanel = new CstmSpeedPanel(this);
+    m_cstSpeedPanel->move(10,30);
+}
+
+QFly::~QFly()
+{
+    delete ui;
+}
---
+++ qfly.h
@@ -0,0 +1,26 @@
+﻿#ifndef QFLY_H
+#define QFLY_H
+
+#include <QWidget>
+#include "CstmBattery.h"
+#include "CstmSpeedPanel.h"
+
+namespace Ui {
+class QFly;
+}
+
+class QFly : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit QFly(QWidget *parent = nullptr);
+    ~QFly();
+
+private:
+    Ui::QFly *ui;
+    CstmBattery * m_cstmBattery;
+    CstmSpeedPanel * m_cstSpeedPanel;
+};
+
+#endif // QFLY_H
